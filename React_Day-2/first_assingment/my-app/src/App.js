9c99dcd8f3e604738a202fe152388371fd73785c
Shubham Kumar Jaiswal <krshubhamjaiswal2000@gmail.com>
2023-03-07 20:58:26 +0530

    REact_Day-2 First Assingment

---
+++ React_Day-2/first_assingment/my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Register here ....</h1>
+      <input placeholder="mobile ..." />
+      <input placeholder="password ..." />
+      <input type="submit" value="Submit" />
+    </div>
+  );
+}
+
+export default App;
