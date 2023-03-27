4edc493ca9666bd8489976fb3bdd306ac43901ab
Shubham Kumar Jaiswal <krshubhamjaiswal2000@gmail.com>
2023-03-27 23:44:39 +0530

    App.js

--- React_10th_Assingment/my-app/src/App.js
+++ React_10th_Assingment/my-app/src/App.js
@@ -1,5 +1,4 @@
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
 import Login from './Components/Login/Login';
 import Navbar from './Components/Navbar/Navbar';
 import SignUp from './Components/SignUp/SignUp';
