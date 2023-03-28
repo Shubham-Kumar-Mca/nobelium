c5244683811b6c86436adfbc0270ed2afb1fbb02
Shubham Kumar Jaiswal <krshubhamjaiswal2000@gmail.com>
2023-03-29 00:39:45 +0530

    Quize Data

---
+++ React_11th_Assingment/my-app/src/data.js
@@ -0,0 +1,94 @@
+const data = [
+    {
+        questionText: 'Which is the capital of France?',
+        answerOption: [
+            { anserText: 'Paris', isCorrect: true },
+            { anserText: 'London', isCorrect: false },
+            { anserText: 'New York', isCorrect: false },
+            { anserText: 'Texas', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Who is the CEO of Tesla?',
+        answerOption: [
+            { anserText: 'Jefff bazos', isCorrect: false },
+            { anserText: 'Elon Musk', isCorrect: true },
+            { anserText: 'Bill gates', isCorrect: false },
+            { anserText: 'Tony Stark', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Which company created Iphone?',
+        answerOption: [
+            { anserText: 'Mango', isCorrect: false },
+            { anserText: 'Panda', isCorrect: false },
+            { anserText: 'Apple', isCorrect: true },
+            { anserText: 'Microsoft', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'How many harry Porters books are there?',
+        answerOption: [
+            { anserText: '1', isCorrect: false },
+            { anserText: '4', isCorrect: false },
+            { anserText: '6', isCorrect: false },
+            { anserText: '7', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'Which City is known as Pink city?',
+        answerOption: [
+            { anserText: 'Jaypur', isCorrect: true },
+            { anserText: 'Purnia', isCorrect: false },
+            { anserText: 'Delhi', isCorrect: false },
+            { anserText: 'Mumbai', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'What is ReactJS?',
+        answerOption: [
+            { anserText: 'Server-side Library', isCorrect: false },
+            { anserText: 'User interface Library', isCorrect: true },
+            { anserText: 'Both A and B', isCorrect: false },
+            { anserText: 'None of the above', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'What was the day of the week on 28th May, 2006?',
+        answerOption: [
+            { anserText: 'Thursday', isCorrect: false },
+            { anserText: 'Friday', isCorrect: false },
+            { anserText: 'Saturday', isCorrect: false },
+            { anserText: 'Sunday', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'Which of the following is used in React.js to increase performance?',
+        answerOption: [
+            { anserText: 'Virtual DOM', isCorrect: true },
+            { anserText: 'Original DOM', isCorrect: false },
+            { anserText: 'Both A and B', isCorrect: false },
+            { anserText: 'None of the above', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Identify the one which is used to pass data to components from outside?',
+        answerOption: [
+            { anserText: 'Render with arguments', isCorrect: false },
+            { anserText: 'setState', isCorrect: false },
+            { anserText: 'propTypes', isCorrect: false },
+            { anserText: 'props', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'Who created React.js?',
+        answerOption: [
+            { anserText: 'Jordan Mike', isCorrect: false },
+            { anserText: 'Jordan Walke', isCorrect: true },
+            { anserText: 'Tim Lee', isCorrect: false },
+            { anserText: 'Jordan Lee', isCorrect: false },
+        ],
+    },
+];
+
+export default data;
