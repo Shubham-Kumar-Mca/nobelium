a2a4c1abf4c6916d90f35716ece39e9beb0b271a
Shubham Kumar Jaiswal <krshubhamjaiswal2000@gmail.com>
2023-03-29 23:17:31 +0530

    Page Component

---
+++ React_12th_Assingment/react-routing-assingment/src/Pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./Comman.module.css"
+
+const About = () => {
+  return (
+    <div className={styles.divTag}>
+        <h2>Welcome to About page</h2>
+    </div>
+  )
+}
+
+export default About
---
+++ React_12th_Assingment/react-routing-assingment/src/Pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./Comman.module.css"
+
+const Login = () => {
+  return (
+    <div className={styles.divTag}>
+        <h2>Welcome to Login page</h2>
+    </div>
+  )
+}
+
+export default Login
